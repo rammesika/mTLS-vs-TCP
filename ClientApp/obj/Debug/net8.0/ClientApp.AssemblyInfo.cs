efeab3141988eac0e3fde392c401bab9fec8715c
rammesika <37574475+rammesika@users.noreply.github.com>
2024-08-20 18:28:05 +0300

    Removed unrelated files

--- ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
+++ ClientApp/obj/Debug/net8.0/ClientApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d509af2c965409fdadfb3196e91dd0f64c61eab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
